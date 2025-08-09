6a4129129225ec95f4caa1db579faa5ee92d0e15
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-09 18:55:16 +0530

    optimal and smart

--- Codeforces/Practice 900/Clock and Strings.cpp
+++ Codeforces/Practice 900/Clock and Strings.cpp
@@ -4,25 +4,19 @@ int main(){
     int t; cin>>t;
     while(t--){
         int a,b,c,d; cin>>a>>b>>c>>d;
-        if(a>b) swap(a,b);
 
-        unordered_set<int> st1;
-        for(int i=a+1;i!=b;i++){
-            i=i%13;
-
-            st1.insert(i);
+        string s;
+        
+        for(int i=1;i<=12;i++){
+            if(i==a || i==b) s+='a';
+            if(i==c || i==d) s+='b';
         }
 
-        unordered_set<int> st2;
-        for(int i=b+1;i!=a;i++){
-            i=i%13;
-            
-            st2.insert(i);
-        }
-
-        if((st1.count(c) && st2.count(d)) || (st1.count(d) && st2.count(c))){
-            cout<<"YES"<<endl;
-        }
-        else cout<<"NO"<<endl;
+        if(s=="abab" || s=="baba"){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
     }
 }
