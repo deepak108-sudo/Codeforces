0a66722f6f4b92ee04787d6f46021ac7b66d29eb
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-13 02:48:08 +0530

    added 1000

---
+++ Practice_1000/Removals Game.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> a(n),b(n);
+
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+
+        vector<int> c=a;
+        reverse(begin(c),end(c));
+
+        if(a==b || c==b){
+            cout<<"Bob";
+        }
+
+        else{
+            cout<<"Alice";
+        }
+        cout<<endl;
+    }
+}
+
