c45871385d3301af14dbff9d868c18dd8a7c6d1b
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-15 00:12:29 +0530

    short and simple

--- Practice_1000/Raspberries.cpp
+++ Practice_1000/Raspberries.cpp
@@ -11,54 +11,19 @@ int main(){
         for(int i=0;i<n;i++)
             cin>>a[i];
 
-        //For all
-            int ans=INT_MAX;
-            for(int i=0;i<n;i++){
-                int temp=a[i]%k;
+        //For all k
+        int ans=INT_MAX;
+        for(int i:a)
+            ans=min(ans,((k-(i%k))%k));
 
-                if(temp==0){
-                    ans=temp;
-                    break;
-                }
-
-                ans=min(ans,(k-temp));
-            }
-        
-            //Special case for 4
-            int four=0;
-        if(k==4 && ans!=0){
-            int mini=INT_MAX;
-            //To find only divisible by 4
-            int zero=0;
-            int one=0;
-            for(int i=0;i<n;i++){
-                int temp=a[i]%2;
-
-                if(temp==0){
-                    zero++;
-                }
-                else if(temp==1){
-                    one++;
-                }
-            }
-            if(zero>=2){
-                four=0;
-            }
-            else if(one>=1 && zero>=1){
-                four=1;
-            }
-            else{
-                four=2;
-            }
-
-        }
-        
+        //To handle 4
         if(k==4){
-            cout<<min(four,ans)<<endl;
+            int even_cnt=count_if(begin(a),end(a),[](int x){ return x%2==0;});
+
+            ans=min(ans,max(0,2-even_cnt));
         }
-        else{
-            cout<<ans<<endl;
-        }
+
+        cout<<ans<<endl;
     }
 }
 
