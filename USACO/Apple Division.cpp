26207001ff87864c37c039b7b8ece0a52ea8b457
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-16 17:34:23 +0530

    modified

--- USACO/Apple Division.cpp
+++ USACO/Apple Division.cpp
@@ -1,27 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
-void solve(vector<int> &p,int i,long long s,long long temp,long long &mini){
-    if(i==(int)p.size()){
-        mini=min(mini,llabs(temp-(s-temp)));
-        return;
+vector<long long> weights;
+
+long long recurse(int i,ll sum1,ll sum2){
+    if(i==weights.size()){
+        return abs(sum1-sum2);
     }
 
-    solve(p,i+1,s,temp+p[i],mini);
-    solve(p,i+1,s,temp,mini);
+    return min(recurse(i+1,sum1+weights[i],sum2),recurse(i+1,sum1,sum2+weights[i]));
 }
+
 
 int main(){
     int n; cin>>n;
-    vector<int> p(n);
+    weights.resize(n);
 
-    //taken input
-    for(int &i:p) { cin>>i; }
-
-    long long s=accumulate(begin(p),end(p),0LL);
-    long long mini=LLONG_MAX;
-
-    solve(p,0,s,0,mini);
-
-    cout<<mini;
+    for(ll &i:weights) cin>>i;
+    cout<<recurse(0,0,0)<<endl;
 }
