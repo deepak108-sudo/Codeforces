6aca3ed198100f9df0a37008f4916bcb72613dd2
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-16 19:05:43 +0530

    added

--- USACO/Apple Division.cpp
+++ USACO/Apple Division.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 using ll=long long;
 
-vector<long long> weights;
-
-long long recurse(int i,ll sum1,ll sum2){
-    if(i==weights.size()){
-        return abs(sum1-sum2);
-    }
-
-    return min(recurse(i+1,sum1+weights[i],sum2),recurse(i+1,sum1,sum2+weights[i]));
-}
-
-
 int main(){
     int n; cin>>n;
-    weights.resize(n);
 
-    for(ll &i:weights) cin>>i;
-    cout<<recurse(0,0,0)<<endl;
+    vector<ll> w(n);
+    for(ll &i:w) cin>>i;
+
+    ll ans=LLONG_MAX;
+    for(int m=0;m<(1<<n);m++){  //For n=3 it will 2^8
+        ll sum1=0,sum2=0;
+        for(int i=0;i<n;i++){
+            if(m & 1<<i){   //if m ith value set then add this in group 1
+                sum1+=w[i];
+            }
+            else{
+                sum2+=w[i];
+            }
+        }
+        ans=min(ans,llabs(sum1-sum2));
+    }
+
+    cout<<ans<<endl;
 }
