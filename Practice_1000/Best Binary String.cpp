3e2bbd953e074ef93acd25f368c5f12b21d1a8e1
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-18 02:29:19 +0530

    optimized and simplied version

--- Practice_1000/Best Binary String.cpp
+++ Practice_1000/Best Binary String.cpp
@@ -5,24 +5,12 @@ int main(){
     int t; cin>>t;
     while(t--){
         string s; cin>>s;
-        int n=s.size();
-
+        
         char prev='0';
-        for(int i=0;i<n;i++){
-            if(s[i]=='?' && prev=='0'){
-                cout<<'0';
-                prev='0';
-            }
-            else if(s[i]=='?' && prev=='1'){
-                cout<<'1';
-                prev='1';
-            }
-            else{
-                cout<<s[i];
-                prev=s[i];
-            }
+        for(auto &c:s){
+            if(c=='?') c=prev;
+            prev=c;
         }
-
-        cout<<endl;
+        cout<<s<<endl;
     }
 }
