ba5fa9709eab49ad066f80f0799097644291a96e
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-19 22:36:28 +0530

    Update future state based on current state

--- USACO/Frog.cpp
+++ USACO/Frog.cpp
@@ -13,12 +13,12 @@ int main(){
     //dp[0] =0 since we are already at first stone
     dp[0]=0;
 
-    for(int i=1;i<n;i++){
-        if(i>=1)
-            dp[i]=min(dp[i],dp[i-1]+abs(h[i-1]-h[i]));
+    for(int i=0;i<n;i++){
+        if(i+1<n)
+            dp[i+1]=min(dp[i+1],dp[i]+abs(h[i+1]-h[i]));
 
-        if(i>=2)
-            dp[i]=min(dp[i],dp[i-2]+abs(h[i-2]-h[i]));
+        if(i+2<n)
+            dp[i+2]=min(dp[i+2],dp[i]+abs(h[i+2]-h[i]));
     }
 
     cout<<dp[n-1]<<endl;
