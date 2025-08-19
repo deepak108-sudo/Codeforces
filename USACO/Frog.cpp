18f6d3651f437b4f477f8c6f8d287daafcf96439
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-19 22:32:10 +0530

    pull dp: Calculate current state based on past state

--- USACO/Frog.cpp
+++ USACO/Frog.cpp
@@ -1,39 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define read(v) for(int &i:v) cin>>i;
-int n;
-vector<int> a;
-vector<int> dp;
-
-int solve(int i){
-    if(i==n-1){
-        return 0;
-    }
-
-    if(dp[i]!=-1){
-        return dp[i];
-    }
-
-    int take1=INT_MAX;
-    int take2=INT_MAX;
-
-    if(i<n-1)
-        take1=abs(a[i+1]-a[i])+solve(i+1);
-
-    if(i<n-2)
-        take2=abs(a[i+2]-a[i])+solve(i+2);
-
-    return dp[i]=min(take1,take2);
-}
-
 
 int main(){
-    cin>>n;
-    a.resize(n);
-    dp.resize(n,-1);
+    int n; cin>>n;
 
-    //take input
-    read(a);
+    vector<int> h(n); 
+    for(int &i:h) cin>>i;
 
-    cout<<solve(0);
+    vector<int> dp(n,INT_MAX);
+
+    //dp[0] =0 since we are already at first stone
+    dp[0]=0;
+
+    for(int i=1;i<n;i++){
+        if(i>=1)
+            dp[i]=min(dp[i],dp[i-1]+abs(h[i-1]-h[i]));
+
+        if(i>=2)
+            dp[i]=min(dp[i],dp[i-2]+abs(h[i-2]-h[i]));
+    }
+
+    cout<<dp[n-1]<<endl;
 }
