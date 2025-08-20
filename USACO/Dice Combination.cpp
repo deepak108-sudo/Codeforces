f64ccb4f4ad8494e6676c3480e81f4ff5ad1c1d9
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-20 10:21:38 +0530

    Added bottom-up

--- USACO/Dice Combination.cpp
+++ USACO/Dice Combination.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<long long> dp;
-
-
-long long solve(long long sum){
-    if(sum==0)  return 1;
-    if(sum<0)   return 0;
-
-    if(dp[sum] != -1) return dp[sum];
-    
-    long long ans=0;
-    for(int i=1;i<=6;i++){
-        ans+=solve(sum-i);
-    }
-
-    return dp[sum]=ans;
-}
+int mod=1e9+7;
 
 int main(){
     int n; cin>>n;
 
-    dp.resize(n+1,-1);
-    cout<<solve(n);
+    dp.resize(n+1,0);
+    dp[0]=1;
+
+    for(int sum=1;sum<=n;sum++){
+        for(int dice=1;dice<=6 ;dice++){
+            if(sum-dice<0) break;
+            dp[sum]=(dp[sum]+dp[sum-dice])%mod;
+
+           // dp[sum]%=mod;
+        }
+    }
+    cout<<dp[n]<<endl;
 }
