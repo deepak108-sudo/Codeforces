30468e78225c8a77c30c4ac9d91c95a4803ac4b4
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-27 04:23:50 +0530

    added 1100 rated

--- Practice_1100/Variety is Discouraged.cpp
+++ Practice_1100/Variety is Discouraged.cpp
@@ -2,59 +2,35 @@
 using namespace std;
 
 void solve(){
-    int n,k; cin>>n>>k;
+   int n; cin>>n;
 
-    vector<int> a(n);
-    
+   vector<int> a(n);
    for(int &i:a) cin>>i;
 
-   //for equal
-   bool equal=all_of(begin(a),end(a),[&](int b){
-    return b==a[0];
-   });
-
-   if(equal==true){
-    cout<<0<<endl;
-    return;
-   }
-
-   //If ataleast two are distinct
    unordered_map<int,int> mp;
-   int maxCount=0;
-   int ans=a[0];
+   for(int &i:a) mp[i]++;
 
-   //Count maximum no.
-   for(int num:a){
-    mp[num]++;
-    if(maxCount<mp[num]){
-        ans=num;
-        maxCount=mp[num];
-    }
-   }
-
-   //process
-   int maxi=0;
-   bool l=false;
-   pair<int,int> p={1,1};
-   pair<int,int> res={1,1};
-   int cnt=0;
+   int l=-1,r=-1,cnt=0,maxi=0;
    for(int i=0;i<n;i++){
-    if(a[i]!=ans && l==false){
-        p.first=i+1;
-        //cnt++;
-        l=true;
-    }else if(a[i]!=ans){
-        p.second=i+1;
-        //cnt++;
-    }else{
-        if((res.second-res.first)<(p.second-p.first)){
-            res=p;
+        if(mp[a[i]]==1){
+            cnt++;
         }
-        //cnt=0;
-        l=false;
-    }
+        else{
+            cnt=0;
+        }
+        if(maxi<cnt){
+            r=i;
+            l=i-cnt+1;
+
+            maxi=cnt;
+        }
    }
-   cout<<res.first<<" "<<res.second<<endl;
+
+   //all are equal i.e [1,1,1,1]
+   if(maxi==0) cout<<0<<endl;
+   else{
+    cout<<l+1<<" "<<r+1<<endl;
+   }
 }
 int main(){
     ios::sync_with_stdio(false);
