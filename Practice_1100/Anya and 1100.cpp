83aeeaf1f8e24c2fb0dc7101e362463cfbf79ca3
deepak108-sudo <deepakkr977140@gmail.com>
2025-08-31 15:00:50 +0530

    updated 1100 rated

--- Practice_1100/Anya and 1100.cpp
+++ Practice_1100/Anya and 1100.cpp
@@ -1,60 +1,53 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
-long long n;
-
-// Function to check if "1100" occurs starting at index i
-bool check_1100(const string &s, long long i) {
-    if (i < 0) return false;
-    if (i >= (long long)s.size() - 3) return false;
-    return (s[i] == '1' && s[i+1] == '1' && s[i+2] == '0' && s[i+3] == '0');
+bool check_1100(string &s,int i){
+    if(i<0) return false;
+    if(i>(int)s.size()-4) return false;
+    
+    if(s[i]=='1' && s[i+1]=='1' && s[i+2]=='0' && s[i+3]=='0') return true;
+    return false;
 }
 
-void solve() {
-    string s;
-    cin >> s;
-    n = s.size();
+void solve(){
+    string s; cin>>s;
+    int n=s.size();
 
-    // Initial count of "1100"
-    long long count = 0;
-    for (long long i = 0; i < n; i++) {
-        if (check_1100(s, i)) count++;
+    int q; cin>>q;
+
+    //count initial 1100
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(check_1100(s,i)) cnt++;
     }
+    
+    while(q--){
+        int i,v; cin>>i>>v;
+        i--;
+        if(s[i]!=('0'+v)){
+            int before=(check_1100(s,i-3) || check_1100(s,i-2) || check_1100(s,i-1) || check_1100(s,i));
 
-    long long q; 
-    cin >> q;
-    while (q--) {
-        long long i, v;
-        cin >> i >> v;
-        i--;  // converting to 0-based index
+            s[i]=('0'+v);
 
-        if (s[i] != char('0' + v)) {
-            // check neighborhood before change
-            bool before = check_1100(s, i-3) || check_1100(s, i-2) 
-                        || check_1100(s, i-1) || check_1100(s, i);
+            int after=(check_1100(s,i-3) || check_1100(s,i-2) || check_1100(s,i-1) || check_1100(s,i));
 
-            // apply the change
-            s[i] = char('0' + v);
-
-            // check neighborhood after change
-            bool after = check_1100(s, i-3) || check_1100(s, i-2) 
-                       || check_1100(s, i-1) || check_1100(s, i);
-
-            count += (after - before);
+            cnt+=(after-before);
+            // if(before!=after){
+            //     if(after) cnt++;
+            //     else cnt--;
+            // }
         }
+        cout<<((cnt>0) ? "YES" : "NO")<<endl;
 
-        cout << (count > 0 ? "YES\n" : "NO\n");
     }
 }
-
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long t;
-    cin >> t;
-    while (t--) {
+    int t; cin>>t;
+    while(t--){
         solve();
     }
-    return 0;
 }
