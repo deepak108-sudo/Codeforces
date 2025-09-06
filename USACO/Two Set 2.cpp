1e691c571311fd22f5fce241f6684cf7a72f64b6
deepak108-sudo <deepakkr977140@gmail.com>
2025-09-06 13:38:37 +0530

    added cses

--- USACO/Two Set 2.cpp
+++ USACO/Two Set 2.cpp
@@ -2,34 +2,30 @@
 using namespace std;
 int mod=1e9+7;
 
-vector<vector<long long>> dp;
-int solve(int total,int i,long long sum,int n){
-    if(i>n) return 0;
-    if(sum==total/2) return 1; 
-
-    if(dp[sum][i]!=-1) return dp[sum][i];
-    
-    int take=solve(total,i+1,sum+i,n);
-    int skip=solve(total,i+1,sum,n);
-
-    return dp[sum][i]=(take+skip)%mod;
-}
-
 
 signed main(){
     int n; cin>>n;
-    bool f=false;
-    
-    long long s=n*(n+1)/2;
+    int s=n*(n+1)/2;
 
-    if(s%2!=0){
+    //hadling odd
+    if(s&1){
         cout<<0<<endl;
-        f=true;
+        return 0;
+    }
+    int target=s/2;
+
+    vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+    dp[0][0]=1;
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=target;j++){
+            dp[i][j]=dp[i-1][j]; //not take;
+
+            int remaining=j-i;
+            if(remaining>=0)
+                dp[i][j]=(dp[i][j] + dp[i-1][remaining])%mod;
+        }
     }
 
-    if(f==false){
-        dp.resize(s+1,vector<long long>(n+1,-1));
-    
-        cout<<solve(s,1,0,n)<<endl;
-    }
+    cout<<dp[n-1][target]<<endl;
 }
