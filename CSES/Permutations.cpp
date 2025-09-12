e3ad1013808c3ade0a936c8ef889ed884950ebbc
deepak108-sudo <deepakkr977140@gmail.com>
2025-09-12 19:34:26 +0530

    modified with most common intuition

--- CSES/Permutations.cpp
+++ CSES/Permutations.cpp
@@ -3,27 +3,22 @@ using namespace std;
 using ll=long long;
 
 int main(){
+    //Intuition: Since every even has distance at 2 and every odd at distance 2 now take care of junction to join.
      ll n; cin>>n;
+    if(n==1){
+        cout<<1;
+        return 0;
+    }
 
      //edge case
      if(n==2 || n==3){
         cout<<"NO SOLUTION"<<endl;
         return 0;
      }
-     if(n==4){
-        cout<<2<<" "<<4<<" "<<1<<" "<<3<<endl;
-        return 0;
+     else{
+        //edge case n=4 when taken odd first
+        for(int i=2;i<=n;i+=2) cout<<i<<" "; //Print all even first with difference 2
+        for(int i=1;i<=n;i+=2) cout<<i<<" "; //Print odd
      }
-
-     int step=(n+1)/2;
-     int temp=n;
-
-     for(int i=0;i<n;i++){
-        cout<<temp<<" ";
-        if(i%2==0){
-            temp-=step;
-        }else{
-            temp+=(step-1);
-        }
-     }
+     
 }
