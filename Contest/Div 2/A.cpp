50ad89dff872c23944986c15ace16eccde6d0e06
deepak108-sudo <deepakkr977140@gmail.com>
2025-09-21 22:31:22 +0530

    to be upsolve

--- Contest/Div 2/A.cpp
+++ Contest/Div 2/A.cpp
@@ -1,25 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll=long long;
 
 void solve(){
-   int a,b; cin>>a>>b;
-    if(a==b){
-        cout<<0<<endl;
-        return;
-    }
+   int n; cin>>n;
 
-   int lcm=(a*b)/__gcd(a,b);
-//    cout<<lcm<<endl;
+   vector<int> a(n);
+
+   for(int &i:a) cin>>i;
+
+   int maxi=*max_element(begin(a),end(a));
+   vector<int> count(maxi+1);
+   
+
+   for(int i=0;i<n;i++){
+    count[a[i]]++;
+   }
+   n=count.size();
+
+
+   vector<int> vec;
+   for(int i=0;i<n;i++){
+    if(count[i]!=0){
+        vec.push_back(count[i]);
+    }
+   }
+
+   n=vec.size();
+   
+   vector<int> sm(n);
+   
+   sm[0]=vec[0];
+   for(int i=1;i<n;i++){
+    sm[i]=min(vec[i],sm[i-1]);
+   }
+
+//    for(int i:sm) cout<<i<<" ";
 //    return;
 
-   if(lcm!=a && lcm!=b){
-    cout<<2<<endl;
-   }
-   else{
-    cout<<1<<endl;
-   }
-}
 
+
+   int mmx=0;
+   for(int i=0;i<n;i++){
+    int sum=0;
+    int num=sm[i];
+    for(int j=i;j>=0;j--){
+        if(sm[j]>=num) sum+=num;
+        mmx=max(mmx,sum);
+        //cout<<mmx<<" ";
+    }
+   }
+//return;
+   cout<<mmx<<endl;
+
+}
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -29,4 +63,3 @@ int main(){
         solve();
     }
 }
-
