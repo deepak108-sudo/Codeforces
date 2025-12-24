7fc86c450b5713c92b812930232b8216e95d933e
deepak108-sudo <deepakkr977140@gmail.com>
2025-12-24 22:36:02 +0530

    Solved successfully

--- CSES/Sorting and Searching/Distinct Values Subarrays.cpp
+++ CSES/Sorting and Searching/Distinct Values Subarrays.cpp
@@ -1,10 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
+#define int long long
 
-//Counting distinct subarrays requires maintaining a window where all elements are unique; checking only the next occurrence of a single element is insufficient because duplicates of other elements can invalidate the subarray earlier. Thats why this solution is wrong
-
-int main(){
+signed main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -13,41 +11,21 @@ int main(){
     for(int &i:a) cin>>i;
     
     //Element and vector of duplicate present
-    unordered_map<int,vector<int>> mp;
-    for(int i=0;i<n;i++){
-        mp[a[i]].push_back(i);
-    }
+    unordered_map<int,int> mp;
 
-    
-    //Traversing
+    int l=0;
     int ans=0;
-    for(int i=0;i<n;i++){
-        auto it=mp.find(a[i]);
-        auto &temp=it->second;
+    for(int r=0;r<n;r++){
+        mp[a[r]]++;
 
-        // cout<<a[i]<<"->";
-        // for(auto e:temp){
-        //     cout<<e<<" ";
-        // }
-        // cout<<endl;
-        
-        //Finding next index which is duplicate of a[i]
-        auto nextIdx=upper_bound(begin(temp),end(temp),a[i])-begin(temp);
-
-        // cout<<a[i]<<" nextIndex"<<nextIdx<<endl;
-        
-        int totalEle=1;
-        if(nextIdx==(int)temp.size()){
-            totalEle=n-i;   //if no duplicate at right hand side
-        }else{
-
-            //total element in right side
-            totalEle=temp[nextIdx]-i+1;
+        while(mp[a[r]]>1){  //means duplicate so shift the l part while it is present
+            mp[a[l]]--;
+            l++;
         }
 
-
-        //Calculating subarray
-        ans+=(totalEle);
+        //No of subarray ending at r is r-l+1 in 0 based index in 1 based r-l
+        ans+=(r-l+1);
     }
     cout<<ans;
+    
 }
