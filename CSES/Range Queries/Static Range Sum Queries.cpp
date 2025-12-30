9af9c39c4ac513c82470d2f7667e1f9f83b58e26
deepak108-sudo <deepakkr977140@gmail.com>
2025-12-30 23:09:50 +0530

    add

---
+++ CSES/Range Queries/Static Range Sum Queries.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+
+int solve(){
+   int n,q; cin>>n>>q;
+   v
+}
+
+
+signed main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
