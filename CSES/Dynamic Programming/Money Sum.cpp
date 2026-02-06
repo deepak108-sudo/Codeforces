32615f41c2ef5e600ad1a2285f1db1f24a0e54f3
deepak108-sudo <deepakkr977140@gmail.com>
2026-02-06 23:51:25 +0530

    Money Sum

--- CSES/Dynamic Programming/Money Sum.cpp
+++ CSES/Dynamic Programming/Money Sum.cpp
@@ -8,34 +8,15 @@ int main(){
    vector<int> a(n);
    for(int &i:a) cin>>i;
 
-   int total=accumulate(begin(a),end(a),0);
+   set<int> st;
+   for(int &num:a){
+        set<int> copy=st;
 
-   vector<vector<bool>> dp(n,vector<bool>(total+1,false));
-
-   dp[0][0]=true;
-   dp[0][a[0]]=true;
-
-   
-   for(int i=1;i<n;i++){
-        for(int curr_sum=0;curr_sum<=total;curr_sum++){
-            dp[i][curr_sum]=dp[i-1][curr_sum];
-            
-            int prev_sum=curr_sum-a[i];
-            if(prev_sum>=0 && dp[i-1][prev_sum]) {
-                dp[i][curr_sum]=true;
-            }
-        }
+        for(int s:copy) st.insert(s+num);
+        st.insert(num);
    }
 
-   //count no. of trues;
-   int cnt=0;
-   for(int sum=1;sum<=total;sum++){
-        if(dp[n-1][sum]) cnt++;
-   }
-
-    //now traverse
-    cout<<cnt<<endl;
-    for(int sum=1;sum<=total;sum++){
-        if(dp[n-1][sum]) cout<<sum<<" ";
-    }
+   //print
+   cout<<st.size()<<endl;
+   for(auto &s:st) cout<<s<<" ";
 }
