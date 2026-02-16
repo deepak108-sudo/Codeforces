cb362a32c73c1a70092e916de10ea879b4d80ca2
deepak108-sudo <deepakkr977140@gmail.com>
2026-02-16 23:55:03 +0530

    solved in contest

---
+++ Contest/Div 3/new/Aaa.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(auto &num:a){
+        if(num%67==0){
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
