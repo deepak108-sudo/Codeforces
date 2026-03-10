7a3fc54f369314b9a2fa053f7df47b204c75d7e0
deepak108-sudo <deepakkr977140@gmail.com>
2026-03-10 23:15:00 +0530

    solved the dp problem of cses

---
+++ CSES/Dynamic Programming/Mountain Range.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n;
+vector<int> a;
+int solve(int i,int prev){
+
+    int total=0;
+    if(i==0){
+        total=1+solve()
+    }
+    else{
+        total=1+solve(i+1,)
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    a.resize(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    solve(0,0);
+}
