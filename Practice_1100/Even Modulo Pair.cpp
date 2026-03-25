96721d28a6957111b70e843920512152fc486d93
deepak108-sudo <deepakkr977140@gmail.com>
2026-03-25 23:47:44 +0530

    small observation

---
+++ Practice_1100/Even Modulo Pair.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin>>n;
+    vector<int> a(n);
+    
+    for(int &i:a) cin>>i;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if((a[j]%a[i])%2==0){
+                cout<<a[i]<<" "<<a[j]<<endl;
+                return;
+            }
+        }
+    }
+    cout<<-1<<endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
+
